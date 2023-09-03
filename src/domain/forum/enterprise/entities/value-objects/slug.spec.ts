342e1355ff2c63986a754c6b7c0cf005975ebab9
Luiz Gustavo <gusstavopdm@gmail.com>
2023-09-03 11:36:02 -0300

    ✅ test(slug.spec.ts): remove console.log statement in slug.spec.ts test to clean up test output

--- src/domain/forum/enterprise/entities/value-objects/slug.spec.ts
+++ src/domain/forum/enterprise/entities/value-objects/slug.spec.ts
@@ -4,7 +4,5 @@ import { Slug } from './slug'
 test('it should be able to create a new slug from text', () => {
   const slug = Slug.createFromText('Example question title')
 
-  console.log(slug)
-
   expect(slug.value).toEqual('example-question-title')
 })
