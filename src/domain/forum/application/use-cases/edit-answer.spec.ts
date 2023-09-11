fbc785f5f93bdff0794e7f9afef5b81b0beea14d
Luiz Gustavo <gusstavopdm@gmail.com>
2023-09-11 11:54:27 -0300

    🔧 fix(answers-repository.ts): add missing methods findById and delete to AnswersRepository interface
    🔧 fix(delete-answer.ts): add missing import for AnswersRepository
    
    🔧 fix(edit-answer.ts): add missing import for Question entity
    
    🔧 fix(edit-question.ts): add missing import for Question entity
    
    🔧 fix(answer.ts): export AnswerProps interface
    
    ✨ feat(delete-answer.spec.ts): add test case to delete an answer
    
    ✨ feat(delete-answer.ts): implement DeleteAnswerUseCase
    
    ✨ feat(edit-answer.spec.ts): add test case to edit an answer
    
    ✨ feat(edit-answer.ts): implement EditAnswerUseCase
    
    ✨ feat(edit-question.ts): add return type to EditQuestionUseCase
    
    ✨ feat(in-memory-answers-repository.ts): implement findById, save, and delete methods

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,53 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { EditAnswerUseCase } from './edit-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      answerId: newAnswer.id.toValue(),
+      content: 'Conteúdo teste',
+    })
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('should be able to edit a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        answerId: 'answer-1',
+        content: 'Edit Answer',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,29 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toValue() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async save(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+  }
+
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
