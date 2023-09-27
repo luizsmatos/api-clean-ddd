b2fbc051059d058d64e3d5b869ddc6795b4a25ac
Luiz Gustavo <gusstavopdm@gmail.com>
2023-09-27 09:54:23 -0300

    🔧 fix(answer-comments-repository.ts): add findById and delete methods to AnswerCommentsRepository interface to support finding and deleting answer comments ✨ feat(delete-answer-comment.ts): implement DeleteAnswerCommentUseCase to delete answer comments based on author and comment id 🔧 fix(in-memory-answer-comments-repository.ts): implement findById and delete methods in InMemoryAnswerCommentsRepository to support finding and deleting answer comments 🔧 fix(delete-answer-comment.spec.ts): update test to use new InMemoryAnswerCommentsRepository and test the delete functionality of DeleteAnswerCommentUseCase ✨ feat(make-answer-comment.ts): add factory function makeAnswerComment to create AnswerComment instances for testing purposes

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -6,6 +6,24 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
   async create(answerComment: AnswerComment): Promise<void> {
     this.items.push(answerComment)
   }
