886fd4517856e3eb881319d90cdb64448a1bd778
Luiz Gustavo <gusstavopdm@gmail.com>
2023-10-04 17:28:43 -0300

    🔧 chore(in-memory-answers-repository.ts): refactor InMemoryAnswersRepository to inject AnswerAttachmentsRepository dependency 🔒 fix(in-memory-answers-repository.ts): delete answer attachments when deleting an answer to ensure data consistency

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,9 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
+  constructor(
+    private answerAttachmentsRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async findById(id: string): Promise<Answer | null> {
     const answer = this.items.find((item) => item.id.toValue() === id)
@@ -40,5 +45,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items.splice(itemIndex, 1)
+    await this.answerAttachmentsRepository.deleteManyByAnswerId(
+      answer.id.toString(),
+    )
   }
 }
